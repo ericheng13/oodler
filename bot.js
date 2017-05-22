8d4cb18672d8d0d223f70838bbf57cf55773e245
ericheng13 <ericheng13@gmail.com>
2017-05-22 10:24:14 -0400

    randomized it

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ for (i = 6; i < str.length; i++) {
    }
    else if (str.substr(i,1) == "e")
    {
-     newstring[i] = h_alph[12];//Math.random()*13];
+     newstring[i] = h_alph[Math.random()*12];
    }
    else if (str.substr(i,1) == "i")
    {
